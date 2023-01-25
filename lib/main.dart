74b34bf61e09b56d949e10916354d9b4f7f1da40
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-01-25 14:03:05 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Startefgfgfr:',
+              'Starter:',
             ),
             Text(
               '$_counter',
