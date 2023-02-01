0131cfbf294225e0cc161ff46b1fa7b0ff0b8866
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-02-01 11:48:52 +0000

    Intro Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,17 @@
 import 'package:flutter/material.dart';
+import 'screens/intro_screen.dart';
 
 void main() {
   runApp(const MyMancho());
 }
 
 class MyMancho extends StatelessWidget {
-  const MyMancho({super.key});
+  const MyMancho({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text('MyManchodfd',
-                  style: TextStyle(color: Colors.black)),
-              backgroundColor: Colors.white54,
-            ),
-            body: Container(
-              decoration: const BoxDecoration(
-                  image: DecorationImage(
-                image: AssetImage('assets/macho.jpg'),
-                fit: BoxFit.cover,
-              )),
-              child: Center(
-                child: Container(
-                    padding: const EdgeInsets.all(15),
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      color: Colors.white60,
-                    ),
-                    child: const Text(
-                      'No Pain, No Gain.',
-                      style: TextStyle(fontSize: 15, shadows: [
-                        Shadow(
-                            offset: Offset(1.0, 1.0),
-                            blurRadius: 2.0,
-                            color: Colors.grey)
-                      ]),
-                    )),
-              ),
-            )));
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: IntroScreen());
   }
 }
---
+++ lib/screens/intro_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title:
+              const Text('MyManchodfd', style: TextStyle(color: Colors.black)),
+          backgroundColor: Colors.white54,
+        ),
+        body: Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage('assets/macho.jpg'),
+            fit: BoxFit.cover,
+          )),
+          child: Center(
+            child: Container(
+                padding: const EdgeInsets.all(15),
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  color: Colors.white60,
+                ),
+                child: const Text(
+                  'No Pain, No Gain.',
+                  style: TextStyle(fontSize: 15, shadows: [
+                    Shadow(
+                        offset: Offset(1.0, 1.0),
+                        blurRadius: 2.0,
+                        color: Colors.grey)
+                  ]),
+                )),
+          ),
+        ));
+  }
+}
