b40c4d84c9824146b9c2d61ebad25aa19aa122a1
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-02-02 15:35:07 +0000

    SingleChildScrollView

--- lib/screens/bmi_screen.dart
+++ lib/screens/bmi_screen.dart
@@ -41,30 +41,34 @@ class _BmiScreenState extends State<BmiScreen> {
         ),
         drawer: const MenuDrawer(),
         bottomNavigationBar: const MenuBottom(),
-        body: Column(
-          children: [
-            ToggleButtons(children: [
-              Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  child: Text('Metric', style: TextStyle(fontSize: fontSize))),
-              Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  child: Text('Imperial', style: TextStyle(fontSize: fontSize)))
-            ], isSelected: isSelected, onPressed: toggleMeasure),
-            TextField(
-                controller: txtHeight,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(hintText: heightMessage)),
-            TextField(
-                controller: txtWeight,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(hintText: weightMessage)),
-            ElevatedButton(
-                onPressed: findBMI,
-                child: Text('Calculate BMI',
-                    style: TextStyle(fontSize: fontSize))),
-            Text(result, style: TextStyle(fontSize: fontSize))
-          ],
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              ToggleButtons(children: [
+                Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    child:
+                        Text('Metric', style: TextStyle(fontSize: fontSize))),
+                Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    child:
+                        Text('Imperial', style: TextStyle(fontSize: fontSize)))
+              ], isSelected: isSelected, onPressed: toggleMeasure),
+              TextField(
+                  controller: txtHeight,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(hintText: heightMessage)),
+              TextField(
+                  controller: txtWeight,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(hintText: weightMessage)),
+              ElevatedButton(
+                  onPressed: findBMI,
+                  child: Text('Calculate BMI',
+                      style: TextStyle(fontSize: fontSize))),
+              Text(result, style: TextStyle(fontSize: fontSize))
+            ],
+          ),
         ));
   }
 
