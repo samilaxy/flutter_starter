3797fffe47bdbccce741a5e2027a743dfdcc2620
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-02-02 17:56:28 +0000

    Update http_helper.dart

--- lib/data/http_helper.dart
+++ lib/data/http_helper.dart
@@ -1,1 +1,6 @@
+import 'package:http/http.dart';
 
+class HttpHelper {
+//11eba8bb941dd6ed614b3014924cfb60
+//https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=11eba8bb941dd6ed614b3014924cfb60
+}
