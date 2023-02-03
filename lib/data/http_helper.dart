a490f5566e23598eb529915abacdc5f03c0ee71a
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-02-03 08:20:32 +0000

    Update http_helper.dart

--- lib/data/http_helper.dart
+++ lib/data/http_helper.dart
@@ -4,9 +4,9 @@ class HttpHelper {
 //11eba8bb941dd6ed614b3014924cfb60
 //https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=11eba8bb941dd6ed614b3014924cfb60
 
-  final String authority = 'api.openweathermap.org/data';
+  final String authority = 'api.openweathermap.org';
   final String path = 'data/2.5/weather';
-  final String apiKey = '1eba8bb941dd6ed614b3014924cfb60';
+  final String apiKey = '11eba8bb941dd6ed614b3014924cfb60';
 
   Future<String> getWeather(String location) async {
     Map<String, dynamic> parameters = {'q': location, 'appid': apiKey};
