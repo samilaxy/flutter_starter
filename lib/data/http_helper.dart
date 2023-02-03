2b12e022a395679eea0648ed3a6e94bbb3ea404f
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-02-03 09:05:55 +0000

    getWeather

--- lib/data/http_helper.dart
+++ lib/data/http_helper.dart
@@ -1,6 +1,6 @@
-import 'dart:convert';
-
+import 'package:flutter_starter/data/weather.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HttpHelper {
 //11eba8bb941dd6ed614b3014924cfb60
@@ -10,12 +10,14 @@ class HttpHelper {
   final String path = 'data/2.5/weather';
   final String apiKey = '11eba8bb941dd6ed614b3014924cfb60';
 
-  Future<String> getWeather(String location) async {
+  Future<Weather> getWeather(String location) async {
     Map<String, dynamic> parameters = {'q': location, 'appid': apiKey};
     Uri uri = Uri.https(authority, path, parameters);
     http.Response result = await http.get(uri);
 
     Map<String, dynamic> data = jsonDecode(result.body);
-    return result.body;
+    Weather weather = Weather.fromJson(data);
+
+    return weather;
   }
 }
