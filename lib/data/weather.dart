3d0f8167e74e67543145badd617944e9e96b5e8c
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-02-03 08:13:42 +0000

    Update weather.dart

---
+++ lib/data/weather.dart
@@ -0,0 +1,10 @@
+import 'dart:ffi';
+
+class Weather {
+  String name = '';
+  String description = '';
+  double temperature = 0;
+  double percieved = 0;
+  int pressure = 0;
+  int humidity = 0;
+}
