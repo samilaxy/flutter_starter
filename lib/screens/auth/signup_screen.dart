c257454821ea5a4e121b6388b620bf1da5a5b520
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-02-27 13:49:53 +0000

    _SignupScreenState

--- lib/screens/auth/signup_screen.dart
+++ lib/screens/auth/signup_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_starter/utils/color_utils.dart';
+import '../../shared/custom_widget.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -9,8 +10,44 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+  final TextEditingController emailTxt = TextEditingController();
+  final TextEditingController passwordTxt = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+            hexStringToColor("F5F5DC"),
+            hexStringToColor("2E3033"),
+            hexStringToColor("21201E"),
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+          child: Center(
+            child: SingleChildScrollView(
+                child: Padding(
+                    padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                    child: Column(children: <Widget>[
+                      logoWidget("assets/logo.png"),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      customTextField("Enter email", Icons.verified_user_sharp,
+                          false, emailTxt),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      customTextField(
+                          "Enter password", Icons.lock, true, passwordTxt),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      customButton(context, "Sign Up", signup)
+                    ]))),
+          )),
+    );
   }
+
+  signup(context, tr) {}
 }
+
+
