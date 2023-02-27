db3de707bbe776adbfc6b8515fcea585bce38647
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-02-27 10:09:01 +0000

    logoWidget

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -13,14 +13,26 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
             hexStringToColor("CB2B93"),
             hexStringToColor("9546C4"),
             hexStringToColor("5E61F4"),
-          ])
-        ),
-      ),
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+          child: SingleChildScrollView(
+              child: Padding(
+                  padding: EdgeInsets.fromLTRB(
+                      20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+                  child: Column(children: <Widget>[
+                    logoWidget("imageName")
+                  ])))),
     );
   }
+}
+
+Image logoWidget(String imageName) {
+  return Image.asset(imageName,
+      fit: BoxFit.fitWidth, 
+      width: 240, height: 240, 
+      color: Colors.white);
 }
