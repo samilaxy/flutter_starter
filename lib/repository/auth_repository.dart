cf3c3783b6b72823c5bf3c1198e081c6afb75537
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-02-28 08:47:41 +0000

    _setInitilaState

--- lib/repository/auth_repository.dart
+++ lib/repository/auth_repository.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter_starter/screens/auth/login_screen.dart';
+import 'package:flutter_starter/screens/intro_screen.dart';
 import 'package:get/get.dart';
 
 class AuthRepository extends GetxController {
@@ -13,5 +13,12 @@ class AuthRepository extends GetxController {
   void onReady() {
     firebaseUser = Rx<User?>(_auth.currentUser);
     firebaseUser.bindStream(_auth.userChanges());
+    ever(firebaseUser, _setInitilaState);
   }
+}
+
+_setInitilaState(User? user) {
+  user == null
+      ? Get.offAll(() => const LoginScreen())
+      : Get.offAll(() => const IntroScreen());
 }
