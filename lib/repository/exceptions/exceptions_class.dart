be2d3fa5d0d9f0f74fa5b13ac205655c86072ca2
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-02-28 09:48:18 +0000

    user-disabled

--- lib/repository/exceptions/exceptions_class.dart
+++ lib/repository/exceptions/exceptions_class.dart
@@ -8,16 +8,22 @@ class SignUpWithEmailAndPasswordFailure {
   factory SignUpWithEmailAndPasswordFailure.code(String code){
 
     switch(code){
+      
       case 'weak-password' :
        return const SignUpWithEmailAndPasswordFailure('Please enter a stronger password.');
+      
       case 'invalid-email' :
        return const SignUpWithEmailAndPasswordFailure('Email is not valid or badly formatted.');
+     
       case 'email-aleady-in-use' :
        return const SignUpWithEmailAndPasswordFailure('An account already exits for that email.');
+      
       case 'operation-not-allowed' :
        return const SignUpWithEmailAndPasswordFailure('Opreation is not allowed. Please contact support.');
-      case 'email-aleady-in-use' :
-       return const SignUpWithEmailAndPasswordFailure('An account already exits for that email.');
+      
+      case 'user-disabled' :
+       return const SignUpWithEmailAndPasswordFailure('This user has been disabled. Plese contact support for help.');
+      
       default: return const SignUpWithEmailAndPasswordFailure();
     }
   }
