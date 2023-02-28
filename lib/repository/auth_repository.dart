0d910274322f0891cb3fc86de8ac4539a789cb3a
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-02-28 08:40:36 +0000

    firebaseUser

--- lib/repository/auth_repository.dart
+++ lib/repository/auth_repository.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class AuthRepository extends GetxController {
   static AuthRepository get instance => Get.find();
 
+  final _auth = FirebaseAuth.instance;
+  late final Rx<User?> firebaseUser;
 
+  @override
+  void onReady() {
+    firebaseUser = Rx<User?>(_auth.currentUser);
+  }
 }
